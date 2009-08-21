0221d5fbcc37470c01ba76018f5daf1a4da408a3
A_Krotov <A_Krotov@6dfe35fe-931f-0410-af5f-a91b034811cd>
2009-08-21 13:56:17 +0000

    Tagged before the lifecycle API rework

---
+++ components/jsr135/tags/jsr135-cr-6860750/src/cldc_application/classes/com/sun/mmedia/MMEventListener.java
@@ -0,0 +1,196 @@
+/*
+ *
+ *  Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License version
+ *  2 only, as published by the Free Software Foundation. 
+ *  
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License version 2 for more details (a copy is
+ *  included at /legal/license.txt). 
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  version 2 along with this work; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA 
+ *  
+ *  Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ *  Clara, CA 95054 or visit www.sun.com if you need additional
+ *  information or have any questions. 
+ */
+package com.sun.mmedia;
+
+import javax.microedition.media.*;
+import com.sun.midp.events.*;
+
+/**
+ * Event listener for events delivered from native layer
+ */
+class MMEventListener implements EventListener {
+
+    /**
+     *  the following constants must be consistent 
+     *  with javacall_media_notification_type enum values
+     *  JAVACALL_EVENT_MEDIA_***, defined in javanotify_multimedia.h
+     */
+    private static final int EVENT_MEDIA_REALIZE_FINISHED        =  1;
+    private static final int EVENT_MEDIA_PREFETCH_FINISHED       =  2;
+    private static final int EVENT_MEDIA_START_FINISHED          =  3;
+    private static final int EVENT_MEDIA_STOP_FINISHED           =  4;
+    private static final int EVENT_MEDIA_DEALLOCATE_FINISHED     =  5;
+    private static final int EVENT_MEDIA_CLOSE_FINISHED          =  6;
+    private static final int EVENT_MEDIA_SET_MEDIA_TIME_FINISHED =  7;
+    private static final int EVENT_MEDIA_DATA_REQUEST            =  8;
+    private static final int EVENT_MEDIA_LENGTH_REQUEST          =  9;
+    private static final int EVENT_MEDIA_DEVICE_AVAILABLE        = 10;
+    private static final int EVENT_MEDIA_DEVICE_UNAVAILABLE      = 11;
+    private static final int EVENT_MEDIA_ERROR                   = 12;
+    private static final int EVENT_MEDIA_END_OF_MEDIA            = 13;
+    private static final int EVENT_MEDIA_DURATION_UPDATED        = 14;
+    private static final int EVENT_MEDIA_BUFFERING_STARTED       = 15;
+    private static final int EVENT_MEDIA_BUFFERING_STOPPED       = 16;
+    private static final int EVENT_MEDIA_RECORD_ERROR            = 17;
+    private static final int EVENT_MEDIA_RECORD_SIZE_LIMIT       = 18;
+    private static final int EVENT_MEDIA_VOLUME_CHANGED          = 19;
+    private static final int EVENT_MEDIA_SYSTEM_VOLUME_CHANGED   = 20;
+    private static final int EVENT_MEDIA_SNAPSHOT_FINISHED       = 21;
+
+    MMEventListener() {
+        MMEventHandler.setListener(this);
+    }
+
+    public boolean preprocess(Event event, Event waitingEvent) {
+        return true;
+    }
+
+    /**
+     * Process an event.
+     * This method will get called in the event queue processing thread.
+     *
+     * @param event event to process
+     */
+    public void process(Event event) {
+        NativeEvent nevt = (NativeEvent)event;
+        HighLevelPlayer p = null;
+
+                if( EventTypes.MMAPI_EVENT != nevt.getType() ) {
+                    return;
+                }
+                
+        switch ( nevt.intParam4 ) {
+        case EVENT_MEDIA_END_OF_MEDIA:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.END_OF_MEDIA, new Long(nevt.intParam2 * 1000));
+            }
+            break;
+
+        case EVENT_MEDIA_DURATION_UPDATED:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.DURATION_UPDATED, new Long(nevt.intParam2 * 1000));
+            }
+            break;
+
+        case EVENT_MEDIA_VOLUME_CHANGED:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                if (nevt.intParam2 < 0) {
+                    nevt.intParam2 = 0;
+                }
+                if (nevt.intParam2 > 100) {
+                    nevt.intParam2 = 100;
+                }
+                p.sendEvent(PlayerListener.VOLUME_CHANGED, new Long(nevt.intParam2));
+            }
+            break;
+
+        // System volume event handler - Send to the all players in this isolate
+        case EVENT_MEDIA_SYSTEM_VOLUME_CHANGED:
+            if (nevt.intParam2 < 0) {
+                nevt.intParam2 = 0;
+            }
+            if (nevt.intParam2 > 100) {
+                nevt.intParam2 = 100;
+            }
+            HighLevelPlayer.sendSystemVolumeChanged(nevt.intParam2);
+            break;
+
+        case EVENT_MEDIA_RECORD_SIZE_LIMIT:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if(p != null) {
+                p.receiveRSL();
+                        }       
+            break;
+        
+        case EVENT_MEDIA_RECORD_ERROR:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.RECORD_ERROR, new String("Unexpected Record Error"));
+            }
+            break;
+
+        case EVENT_MEDIA_BUFFERING_STARTED:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.BUFFERING_STARTED, new Long(nevt.intParam2 * 1000));
+            }
+            break;
+        
+        case EVENT_MEDIA_BUFFERING_STOPPED:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.BUFFERING_STOPPED, new Long(nevt.intParam2 * 1000));
+            }
+            break;
+
+        case EVENT_MEDIA_ERROR:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.sendEvent(PlayerListener.ERROR, new String("Unexpected Media Error"));
+            }
+            break;
+
+        case EVENT_MEDIA_DATA_REQUEST:
+            p = HighLevelPlayer.get(nevt.intParam1);
+            DirectDebugOut.nDebugPrint("Java: MMEventListener: Data request");
+            if (p != null) {
+                p.continueDownload();
+            }
+            break;
+
+        case EVENT_MEDIA_SET_MEDIA_TIME_FINISHED:
+            long time = ( long )nevt.intParam2 * 1000;
+            p = HighLevelPlayer.get(nevt.intParam1);
+            if (p != null) {
+                p.notifyMediaTimeSet(time);
+            }
+        case EVENT_MEDIA_REALIZE_FINISHED:
+        case EVENT_MEDIA_PREFETCH_FINISHED:
+        case EVENT_MEDIA_START_FINISHED:
+        case EVENT_MEDIA_STOP_FINISHED:
+        case EVENT_MEDIA_DEALLOCATE_FINISHED:
+        case EVENT_MEDIA_CLOSE_FINISHED:
+            if( null == p ) {
+                p = HighLevelPlayer.get(nevt.intParam1);
+            }
+            if (p != null) {
+                p.unblockOnEvent( 0 == nevt.intParam2 ); // JAVACALL_OK is 0 !
+            }
+            break;
+         
+        case EVENT_MEDIA_SNAPSHOT_FINISHED:
+            p = HighLevelPlayer.get( nevt.intParam1 );
+            if( null != p )
+            {
+                p.notifySnapshotFinished();
+            }
+            break;
+                
+        }
+    }
+}
---
+++ components/jsr135/tags/jsr135-cr-6860750/src/share/components/direct-player/classes/com/sun/mmedia/DirectInputThread.java
@@ -0,0 +1,170 @@
+/*
+ *
+ * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 only, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is
+ * included at /legal/license.txt).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this work; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
+ * Clara, CA 95054 or visit www.sun.com if you need additional
+ * information or have any questions.
+ */
+
+package com.sun.mmedia;
+
+import java.io.IOException;
+import javax.microedition.media.MediaException;
+import javax.microedition.media.protocol.SourceStream;
+import java.lang.ref.WeakReference;
+
+class DirectInputThread extends Thread {
+
+    //Caution: these fields are used by native (read-write) !!!
+    private volatile long curPos = 0;
+    private volatile long posToRead = 0;
+    private volatile int lenToRead = 0;
+
+    private boolean requestPending = false;
+
+    private WeakReference wrPlayer;
+
+    private byte[] tmpBuf = new byte [ 0x1000 ];  // 4 Kbytes
+    private volatile boolean isDismissed = false;
+
+    private final Object requestLock = new Object();
+
+    DirectInputThread(HighLevelPlayer p) throws MediaException {
+        long len = p.stream.getContentLength();
+        if( -1 != len ) {
+            nNotifyStreamLen( p.getNativeHandle(), len );
+        }
+        else {
+            throw new MediaException(
+                    "Cannot playback stream with unknown length" );
+        }
+        wrPlayer = new WeakReference( p );
+    }
+    
+
+    private native void nWriteData( byte [] buf, int len, int handle );
+    private native void nGetRequestParams( int handle );
+    private native void nNotifyEndOfStream( int handle );
+    private native void nNotifyStreamLen( int handle, long len );
+
+    public void run(){
+
+mainloop:
+        for(;;) {
+            HighLevelPlayer owner = (HighLevelPlayer) wrPlayer.get();
+            if (null == owner || isDismissed ) {
+                return;
+            }
+
+            synchronized( requestLock ) {
+                while ( !requestPending )
+                {
+                    try {
+                        requestLock.wait(1000);
+                    } catch (InterruptedException ex) {}
+                    if( isDismissed ) {
+                        return;
+                    }
+
+                    owner = (HighLevelPlayer) wrPlayer.get();
+                    if (null == owner) {
+                        return;
+                    }
+
+                }
+                requestPending = false;
+            }
+
+            nGetRequestParams( owner.getNativeHandle() );
+
+            while( 0 < lenToRead ) {
+                int read = 0;
+                
+                if( isDismissed ) {
+                    return;
+                }
+
+                try {
+                    seek( owner.stream );
+
+                    if( isDismissed ) {
+                        return;
+                    }
+
+                    int len = lenToRead > tmpBuf.length ?
+                                    tmpBuf.length : lenToRead;
+                    read = owner.stream.read(tmpBuf, 0, len);
+                } catch ( MediaException ex) {
+                    //owner.abort( ex.getMessage() );
+                    return;
+                } catch ( IOException e ) {
+//                    owner.abort("Stream reading IOException: "
+//                            + e.getMessage() );
+                    return;
+                }
+
+                owner = (HighLevelPlayer) wrPlayer.get();
+                if ( null == owner || isDismissed ) {
+                    return;
+                }
+                
+                if( -1 == read ) {
+                    nNotifyEndOfStream( owner.getNativeHandle() );
+                }
+                else {
+                    nWriteData( tmpBuf, read, owner.getNativeHandle() );
+                }
+            }
+        }
+    }
+
+    private void seek( SourceStream stream ) throws IOException, MediaException {
+        if( stream.tell() == posToRead ) {
+            return;
+        }
+        if( stream.getSeekType() ==
+                SourceStream.NOT_SEEKABLE ||
+            ( posToRead != 0 &&
+              stream.getSeekType() !=
+                SourceStream.RANDOM_ACCESSIBLE ) ) {
+            throw new MediaException("The stream is not seekable");
+        }
+
+        stream.seek(posToRead);
+    }
+
+    public void requestData() {
+        synchronized( requestLock ) {
+            requestPending = true;
+            requestLock.notify();
+        }
+    }
+
+    public void close() {
+        isDismissed = true;
+        synchronized (requestLock) {
+            requestLock.notify();
+        }
+        try {
+            this.join();
+        } catch (InterruptedException ex) {}
+    }
+
+}
